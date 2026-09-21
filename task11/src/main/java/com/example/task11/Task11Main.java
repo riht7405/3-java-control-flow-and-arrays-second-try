a27859893a9f496cf91a78faabe6a42a3299c1b5
riht7405 <fly200fly@gmail.com>
2026-09-22 03:27:48 +0500

    Module 2 - task11

--- task11/src/main/java/com/example/task11/Task11Main.java
+++ task11/src/main/java/com/example/task11/Task11Main.java
@@ -12,7 +12,20 @@ public class Task11Main {
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr == null || arr.length == 0) {
+            return;
+        }
+
+        int minIndex = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[minIndex]) {
+                minIndex = i;
+            }
+        }
+
+        int temp = arr[0];
+        arr[0] = arr[minIndex];
+        arr[minIndex] = temp;
     }
 
 }
